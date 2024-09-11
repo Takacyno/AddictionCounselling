555815506b35dd95784d57c4977e4ea68bbf4221
Takacyno <taka2001yuki@gmail.com>
2024-09-11 21:29:52 +0900

    a

--- Counselling/DBconnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    $DBhost='133.18.244.234';
-    $DBusername='home10';
-    $DBpassword='8940hakuyo';
-    $link=mysqli_connect($DBhost,$DBusername,$DBpassword);
-    $db=mysqli_select_db($link,"takayuki");
-?>
