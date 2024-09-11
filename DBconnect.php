6b25fa7a44cd71d4c6097f07f5a70a9afefe1fe1
Takacyno <taka2001yuki@gmail.com>
2024-09-11 21:23:22 +0900

    Revert "a"
    This reverts commit 70dcd8e2c02e3fe64af69b00a2a8c840a6e8f0b6.

--- DBconnect.php
+++ DBconnect.php
@@ -1,8 +1,8 @@
 <?php
     session_start();
-    $DBhost='localhost';
-    $DBusername='root';
-    $DBpassword='local';
+    $DBhost='133.18.244.234';
+    $DBusername='home10';
+    $DBpassword='8940hakuyo';
     $link=mysqli_connect($DBhost,$DBusername,$DBpassword);
-    $db=mysqli_select_db($link,"local");
+    $db=mysqli_select_db($link,"takayuki");
 ?>
